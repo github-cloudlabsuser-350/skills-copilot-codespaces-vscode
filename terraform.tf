61993e029f5655d1c68cca1fec1d56e7e34362f4
github-cloudlabsuser-350 <153869971+github-cloudlabsuser-350@users.noreply.github.com>
2024-05-24 09:23:39 +0000

    Copilot third commit

---
+++ terraform.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "storageAccountName" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storageAccountName
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
